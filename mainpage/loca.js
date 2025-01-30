c76993e7d31db770307603990aac8a101da3fdcf
Govind9066 <gadekargovind575@gmail.com>
2025-01-30 17:00:27 +0530

    adding cards

--- mainpage/loca.js
+++ mainpage/loca.js
@@ -1,6 +1,27 @@
-const search=document.querySelector(".search_cont");
-const buy_btn=document.querySelector(".buy_btn");
+const search = document.querySelector(".search_cont");
+const buy_btn = document.querySelector(".buy_btn");
 
-buy_btn.addEventListener("click",()=>{
-    
+buy_btn.addEventListener("click", () => {
+
 });
+
+
+
+function catagory_sec(img, product_name){
+    let category_section = document.querySelector(".category-section");
+    const cat_items=document.createElement("div").classList.add("catagory-product");
+    cat_items.innerHtml =
+                    `<div class="catagory-item">
+                        <img src="${img}" alt="Electronics">
+                        <p>${product_name}</p>
+                    </div>`;
+                    category_section.appendChild(cat_items);
+}
+
+catagory_sec(
+    "Images/electronic-product1.png",
+    "Electronics"
+);
+
+
+
